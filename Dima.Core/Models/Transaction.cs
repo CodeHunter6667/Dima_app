610397b9436cbb3daf964bb05eafd1f115c94cdd
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-08-22 17:30:45 -0300

    commit inicial

---
+++ Dima.Core/Models/Transaction.cs
@@ -0,0 +1,16 @@
+using Dima.Core.Enums;
+
+namespace Dima.Core.Models;
+
+public class Transaction
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? PaidOrReceivedAt { get; set; }
+    public ETransactionType Type { get; set; } = ETransactionType.WITHDRAW;
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+    public string UserId { get; set; } = string.Empty;
+}
---
+++ Dima.Core/Requests/RequestBase.cs
@@ -0,0 +1,6 @@
+namespace Dima.Core.Requests;
+
+public class RequestBase
+{
+    public string UserId { get; set; }
+}
