85c0a927b837fd864057f402a5002aa837df14af
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-08-28 17:44:27 -0300

    feat: padronizacão de Requests para Category

---
+++ Dima.Core/Requests/Categories/GetCategoryByIdRequest.cs
@@ -0,0 +1,6 @@
+namespace Dima.Core.Requests.Categories;
+
+public class GetCategoryByIdRequest : RequestBase
+{
+    public long Id { get; set; }
+}
---
+++ Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Requests.Categories;
+
+public class UpdateCategoryRequest : RequestBase
+{
+    public long Id { get; set; }
+    [Required(ErrorMessage = "Titulo é obrigatório")]
+    [Range(5,80,ErrorMessage = "Título deve ter entre 5 e 80 caracteres")]
+    public string Title { get; set; }
+    [Required(ErrorMessage = "Descricão é obrigatório")]
+    [MaxLength(255, ErrorMessage = "Descricão deve ter no máximo 255 caracteres")]
+    public string Description { get; set; }
+    [Required(ErrorMessage = "Usuário é obrigatório")]
+    public string UserId { get; set; }
+}
