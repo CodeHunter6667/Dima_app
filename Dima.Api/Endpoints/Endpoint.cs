3d2b89369337d543dd70ae2f6052a4d2d3b1fdbf
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-11-01 20:52:30 -0300

    agrupando e mapeando os endpoints

---
+++ Dima.Api/Endpoints/Endpoint.cs
@@ -0,0 +1,26 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Api.Endpoints.Categories;
+using Dima.Api.Migrations;
+
+namespace Dima.Api.Endpoints;
+
+public static class Endpoint
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        var endpoint = app
+            .MapGroup("");
+
+        endpoint.MapGroup("v1/categories")
+            .WithTags("Categories")
+            .RequireAuthorization()
+            .MapEndpoint<CreateCategoryEndpoint>()
+            ;
+    }
+
+    private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app) where TEndpoint : IEndpoint
+    {
+        TEndpoint.Map(app);
+        return app;
+    }
+}
