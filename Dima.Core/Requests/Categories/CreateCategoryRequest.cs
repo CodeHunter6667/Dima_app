8c17e4487332f85d1d57d019cf0bbcd8b15f0b72
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-11-01 19:37:18 -0300

    alterações de inicialização dos requests

--- Dima.Core/Requests/Categories/CreateCategoryRequest.cs
+++ Dima.Core/Requests/Categories/CreateCategoryRequest.cs
@@ -7,10 +7,10 @@ public class CreateCategoryRequest : RequestBase
     public long Id { get; set; }
     [Required(ErrorMessage = "Titulo é obrigatório")]
     [Range(5,80,ErrorMessage = "Título deve ter entre 5 e 80 caracteres")]
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
     [Required(ErrorMessage = "Descricão é obrigatório")]
     [MaxLength(255, ErrorMessage = "Descricão deve ter no máximo 255 caracteres")]
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty;
     [Required(ErrorMessage = "Usuário é obrigatório")]
-    public string UserId { get; set; }
+    public string UserId { get; set; } = string.Empty;
 }
--- Dima.Core/Requests/RequestBase.cs
+++ Dima.Core/Requests/RequestBase.cs
@@ -2,5 +2,5 @@ namespace Dima.Core.Requests;
 
 public abstract class RequestBase
 {
-    public string UserId { get; set; }
+    public string UserId { get; set; } = string.Empty;
 }
