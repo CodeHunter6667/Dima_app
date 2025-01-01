c6d6575531896ffe4a85fc92b1a50fe1245e0837
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-01-01 14:01:07 -0300

    feat: finalizados endpoints de Category

---
+++ Dima.Api/Endpoints/Categories/GetCategoryByIdEndpoint.cs
@@ -0,0 +1,35 @@
+
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Categories;
+
+public class GetCategoryByIdEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapGet("/{id}", HandleAsync)
+            .WithName("Categories: Get By Id")
+            .WithSummary("Recupera uma categoria")
+            .WithDescription("Recupera uma categoria")
+            .WithOrder(4)
+            .Produces<Response<Category?>>();
+
+    public static async Task<IResult> HandleAsync(
+        ICategoryHandler handler,
+        long id)
+    {
+        var request = new GetCategoryByIdRequest
+        {
+            Id = id,
+            UserId = "teste@balta.io"
+        };
+        
+        var result = await handler.GetAsync(request);
+        return result.IsSuccess
+            ? Results.Ok(result.Data)
+            : Results.BadRequest(result.Data);
+    }
+}
