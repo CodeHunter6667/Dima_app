46c88665a3d6a24569c879131ab990011618da80
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-01-03 17:00:27 -0300

    feat: organizando program

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,42 +1,25 @@
-using Dima.Api.Data;
+using System.Security.Claims;
+using Dima.Api;
+using Dima.Api.Common.Api;
 using Dima.Api.Endpoints;
-using Dima.Api.Handlers;
 using Dima.Api.Models;
-using Dima.Core.Handlers;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services
-    .AddAuthentication(IdentityConstants.ApplicationScheme)
-    .AddIdentityCookies();
-builder.Services.AddAuthorization();
-
-var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
-builder.Services.AddDbContext<AppDbContext>(
-    options => options.UseSqlServer(connectionString)
-);
-
-builder.Services.AddIdentityCore<User>()
-    .AddRoles<IdentityRole<long>>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddApiEndpoints();
-
-builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(
-    x => x.CustomSchemaIds(n => n.FullName)
-);
+builder.AddConfiguration();
+builder.AddSecurity();
+builder.AddDataContexts();
+builder.AddCrossOrigin();
+builder.AddDocumantation();
+builder.AddServices();
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+if(app.Environment.IsDevelopment())
+    app.ConfigureDevEnvironment();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-app.MapGet("/", () => new { message = "OK" });
+app.UseCors(ApiConfiguration.CorsPolicyName);
+app.UseSecurity();
 app.MapEndpoints();
+
 app.Run();
